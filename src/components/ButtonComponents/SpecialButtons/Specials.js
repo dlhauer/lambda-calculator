76da9471bdd617f1039bf617a9018257f5c22039
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-05 14:06:23 -0800

    Completed number and special buttons

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import SpecialButton from './SpecialButton';
 
 //import any components needed
 
@@ -7,13 +8,14 @@ import {specials} from '../../../data';
 console.log(specials);
 
 const Specials = () => {
-  // STEP 2 - add the imported data to state
   const [specialState, setSpecialState] = useState(specials);
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {specialState.map( (item, index) => (
+        <SpecialButton key={index} button={item}/>
+      ))}
     </div>
   );
 };
+
+export default Specials;
