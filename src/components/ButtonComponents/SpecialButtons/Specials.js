bf0bfc2ca9ab170f3f60ce19eeb1094b0994ecf7
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-11-05 13:54:36 -0800

    Completed operator buttons

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
@@ -8,7 +8,7 @@ console.log(specials);
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [specialState, setSpecialState] = useState(specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
